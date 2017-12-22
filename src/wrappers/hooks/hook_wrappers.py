ac727d3802115300624b2ca5a6392517288ab2e3
Ashley Gould <agould@ucop.edu>
2017-12-22 15:31:01 -0800

    new sceptremods.template: ecs_fargate; work in progress
    also:
     template wrapper for ecs_fargate
     new hook ecs_cluster with wrapper

--- src/wrappers/hooks/hook_wrappers.py
+++ src/wrappers/hooks/hook_wrappers.py
@@ -8,15 +8,16 @@ calling hooks via python sys.path.
 
 from sceptremods.hooks.route53 import Route53HostedZone
 class Route53HostedZone(Route53HostedZone):
-    """Wrapper class for sceptremods.hooks.route53.Route53HostedZone"""
     pass
 
 from sceptremods.hooks.acm_certificate import AcmCertificate
 class AcmCertificate(AcmCertificate):
-    """Wrapper class for sceptremods.hooks.acm_certificate.AcmCertificate"""
     pass
 
 from sceptremods.hooks.account_verifier import AccountVerifier
 class AccountVerifier(AccountVerifier):
-    """Wrapper class for sceptremods.hooks.account_verifier.AccountVerifier"""
+    pass
+
+from sceptremods.hooks.ecs_cluster import ECSCluster
+class AccountVerifier(ECSCluster):
     pass
