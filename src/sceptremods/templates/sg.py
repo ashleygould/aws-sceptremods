5ff55fb7c711846b0367d973f85fedf083277832
Ashley Gould <agould@ucop.edu>
2018-01-31 16:44:22 -0800

    rework sg.py to create arbitrary arrangment of Sec groups

--- src/sceptremods/templates/sg.py
+++ src/sceptremods/templates/sg.py
@@ -1,10 +1,20 @@
+"""
+Assumptions:
+    security groups reside in a VPC
+    only ipv4 ip and cidr addresses
+    no egress rules, only ingress rules
+"""
+
 
 import sys
 
 from troposphere import (
+    AccountId,
     GetAtt,
     Join,
+    NoValue,
     Output,
+    Region,
     Ref,
     ec2,
 )
@@ -20,21 +30,29 @@ class SG(BaseTemplate):
     VARSPEC = {
         'VpcId': {
             'type': str,
-            'description': 'ID of the VPC to use for ecs service.',
-            'default': 'bogus-VpcId-for-testing-only',
+            'description': 'ID of the VPC where to define security groups',
+            'default': 'BOGUS-VPCID-FOR-TESTING-ONLY',
         },
-        'PublicPortMap': {
+        'SecurityGroups': {
             'type': list,
+            'description': 'List of dictionaries of EC2 SecurityGroup parameters',
             'default': [
-                dict(Port='80', Protocol='tcp'),
-                dict(Port='443', Protocol='tcp'),
+                dict(
+                    name='PublicSG',
+                    description='allow inbound traffic from internet on specified ports',
+                    ingress_rules=[
+                        dict(port='80', proto='tcp', source_ip='0.0.0.0/0'),
+                        dict(port='443', proto='tcp', source_ip='0.0.0.0/0'),
+                    ],
+                ),
+                dict(
+                    name="PrivateSG",
+                    description='allow inbound traffic from public security group on any port',
+                    ingress_rules=[
+                        dict(source_sg='PublicSG'),
+                    ],
+                ),
             ],
-            'description': 'List of port mappings to allow through public security group',
-        },
-        'PublicCidr': {
-            'type': str,
-            'default': '0.0.0.0/0',
-            'description': 'The Cidr address from which clients can access the public security group',
         },
     }
 
@@ -42,41 +60,47 @@ class SG(BaseTemplate):
         self.vars = self.validate_user_data()
         t = self.template
 
-        public_ingress_rules = list()
-        for mapping in self.vars['PublicPortMap']:
-            public_ingress_rules.append(ec2.SecurityGroupRule(
-                IpProtocol=mapping.get('Protocol', 'tcp'),
-                ToPort=mapping['Port'],
-                FromPort=mapping['Port'],
-                CidrIp=self.vars['PublicCidr'],
+        self.security_groups = dict()
+        for sg_params in self.vars['SecurityGroups']:
+            if 'ingress_rules' in sg_params:
+                ingress_rules = list()
+
+                # munge ingress rule attributes
+                r_attr = dict()
+                for rule in sg_params['ingress_rules']:
+
+                    if 'source_sg' in rule and rule['source_sg']:
+                        r_attr['SourceSecurityGroupId'] = Ref(rule['source_sg'])
+
+                    if 'source_ip' in rule and rule['source_ip']:
+                        r_attr['CidrIp'] = rule['source_ip']
+
+                    if 'port' in rule and rule['port']:
+                        if str(rule['port']).rfind('-') != -1:
+                            r_attr['ToPort'], r_attr['FromPort'] = str(rule['port']).split('-')
+                        else:
+                            r_attr['ToPort'] = str(rule['port'])
+                            r_attr['FromPort'] = str(rule['port'])
+
+                    if not 'proto' in rule:
+                        r_attr['IpProtocol'] = '-1'
+                    else:
+                        r_attr['IpProtocol'] = rule['proto']
+
+                    ingress_rules.append(ec2.SecurityGroupRule(**r_attr))
+
+            self.security_groups[sg_params['name']] = t.add_resource(ec2.SecurityGroup(
+                sg_params['name'],
+                VpcId=self.vars['VpcId'],
+                GroupDescription=sg_params['description'],
+                SecurityGroupIngress=ingress_rules,
             ))
-        public_security_group = t.add_resource(ec2.SecurityGroup(
-            "PublicSecurityGroup",
-            VpcId=self.vars['VpcId'],
-            GroupDescription='allow inbound traffic from internet on specified ports',
-            SecurityGroupIngress=public_ingress_rules,
-        ))
 
-        private_security_group = t.add_resource(ec2.SecurityGroup(
-            "PrivateSecurityGroup",
-            VpcId=self.vars['VpcId'],
-            GroupDescription='allow inbound traffic from public security group on any port',
-            SecurityGroupIngress=[ec2.SecurityGroupRule(
-                IpProtocol='-1',
-                SourceSecurityGroupId=Ref(public_security_group),
-            )]
-        ))
-
-        t.add_output(Output(
-            "PublicSecurityGroup",
-            Description="Public Security Group",
-            Value=Ref(public_security_group)
-        ))
-        t.add_output(Output(
-            "PrivateSecurityGroup",
-            Description="Private Security Group",
-            Value=Ref(private_security_group)
-        ))
+            t.add_output(Output(
+                sg_params['name'],
+                Description=sg_params['description'],
+                Value=Ref(self.security_groups[sg_params['name']])
+            ))
 
 
 #
