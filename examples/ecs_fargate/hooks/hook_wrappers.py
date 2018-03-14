c6a311b1ee12722bf5849ab3c9aefddb24bf5157
Ashley Gould <agould@ucop.edu>
2018-03-14 13:58:43 -0700

    update ecs_fargate example re s3_bucket delete hook

--- examples/ecs_fargate/hooks/hook_wrappers.py
+++ examples/ecs_fargate/hooks/hook_wrappers.py
@@ -3,7 +3,10 @@ within sceptre projects.  Each class in this module acts as a pass-though class
 to a real custom hook.
 
 This is an unfortunate hack required because sceptre does not currently support
-calling hooks via python sys.path.
+calling hooks via python sys.path.  Sceptre v2 resolves this issue, but until
+then...
+
+See: https://github.com/cloudreach/sceptre/wiki/Roadmap
 """
 
 from sceptremods.hooks.route53 import Route53HostedZone
@@ -19,9 +22,17 @@ class AccountVerifier(AccountVerifier):
     pass
 
 from sceptremods.hooks.ecs_cluster import ECSCluster
-class AccountVerifier(ECSCluster):
+class ECSCluster(ECSCluster):
     pass
  
 from sceptremods.hooks.ecs_task_exec_role import ECSTaskExecRole
-class AccountVerifier(ECSTaskExecRole):
+class ECSTaskExecRole(ECSTaskExecRole):
+    pass
+ 
+from sceptremods.hooks.s3_bucket import S3Bucket
+class S3Bucket(S3Bucket):
+    pass
+
+from sceptremods.hooks.experimental.zappa_lambda import ZappaLambda
+class ZappaLambda(ZappaLambda):
     pass
